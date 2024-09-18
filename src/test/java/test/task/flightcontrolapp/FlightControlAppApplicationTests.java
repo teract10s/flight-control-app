e89b2d1431b9b0064453a52a90a810177ebd1e93
Taras Leonovet <taras.leonovets.job@gmail.com>
2024-09-18 10:10:10 +0300

    initial commit

---
+++ src/test/java/test/task/flightcontrolapp/FlightControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.flightcontrolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightControlAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
